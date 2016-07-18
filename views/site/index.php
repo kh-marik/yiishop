3da36dfdf5fe2293fce37463a0d4805df1e45ce1
Marik <zombie_squad@mail.ru>
2016-07-18 17:11:09 +0500

    Edit images paths

--- views/site/index.php
+++ views/site/index.php
@@ -21,13 +21,13 @@
                         </ol>
                         <div class="carousel-inner">
                             <div class="item active">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="/images/carousel//800x300.png" alt="">
                             </div>
                             <div class="item">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="/images/carousel//800x300.png" alt="">
                             </div>
                             <div class="item">
-                                <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                <img class="slide-image" src="/images/carousel//800x300.png" alt="">
                             </div>
                         </div>
                         <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
@@ -43,7 +43,7 @@
             <div class="row">
                 <div class="col-sm-4 col-lg-4 col-md-4">
                     <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
+                        <img src="/images/products/320x150.png" alt="">
                         <div class="caption">
                             <h4 class="pull-right">$24.99</h4>
                             <h4><a href="#">First Product</a>
@@ -65,7 +65,7 @@
                 </div>
                 <div class="col-sm-4 col-lg-4 col-md-4">
                     <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
+                        <img src="/images/products/320x150.png" alt="">
                         <div class="caption">
                             <h4 class="pull-right">$64.99</h4>
                             <h4><a href="#">Second Product</a>
@@ -86,7 +86,7 @@
                 </div>
                 <div class="col-sm-4 col-lg-4 col-md-4">
                     <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
+                        <img src="/images/products/320x150.png" alt="">
                         <div class="caption">
                             <h4 class="pull-right">$74.99</h4>
                             <h4><a href="#">Third Product</a>
@@ -107,7 +107,7 @@
                 </div>
                 <div class="col-sm-4 col-lg-4 col-md-4">
                     <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
+                        <img src="/images/products/320x150.png" alt="">
                         <div class="caption">
                             <h4 class="pull-right">$84.99</h4>
                             <h4><a href="#">Fourth Product</a>
@@ -128,7 +128,7 @@
                 </div>
                 <div class="col-sm-4 col-lg-4 col-md-4">
                     <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
+                        <img src="/images/products/320x150.png" alt="">
                         <div class="caption">
                             <h4 class="pull-right">$94.99</h4>
                             <h4><a href="#">Fifth Product</a>
@@ -148,12 +148,25 @@
                     </div>
                 </div>
                 <div class="col-sm-4 col-lg-4 col-md-4">
-                    <h4><a href="#">Like this template?</a>
-                    </h4>
-                    <p>If you like this template, then check out
-                        <a target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this tutorial</a> on how to build a working review system for your online store!
-                    </p>
-                    <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                    <div class="thumbnail">
+                        <img src="/images/products/320x150.png" alt="">
+                        <div class="caption">
+                            <h4 class="pull-right">$94.99</h4>
+                            <h4><a href="#">Fifth Product</a>
+                            </h4>
+                            <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="ratings">
+                            <p class="pull-right">18 reviews</p>
+                            <p>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
