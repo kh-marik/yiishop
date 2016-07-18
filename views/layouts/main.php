2760e0e1cfe65214f7b9050f83317197ba927591
Marik <zombie_squad@mail.ru>
2016-07-18 17:03:05 +0500

    Initial commit

---
+++ views/layouts/main.php
@@ -0,0 +1,74 @@
+<?php
+    use yii\helpers\Html;
+    use yii\bootstrap\Nav;
+    use yii\bootstrap\NavBar;
+    use yii\widgets\Breadcrumbs;
+    use app\assets\AppAsset;
+    use app\assets\ieAppAsset;
+
+    AppAsset::register($this);
+    ieAppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+
+    <!DOCTYPE html>
+    <html lang="<?=Yii::$app->language?>">
+    <head>
+        <meta charset="<?=Yii::$app->charset?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?=Html::csrfMetaTags()?>
+        <title><?=Html::encode($this->title)?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Start Bootstrap</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <?= $content; ?>
+
+        <div class="container">
+            <hr>
+            <!-- Footer -->
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>&copy; YiiShop <?=date('Y')?></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <!-- /.container -->
+        <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
